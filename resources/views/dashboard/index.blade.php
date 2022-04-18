d7e47141a616c2d65b89759b6df0fd7e6c34600e
nouran tariq <noo0ur.264@gmail.com>
2022-04-18 14:00:52 +0200

    dashboard

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.dashboard.app')
+@section('content')
+    <div class="content-wrapper">
+        <section class="content-header">
+            <h1>Blank Page
+            <small> it all starts here</small>
+            </h1>
+
+
+        </section >
+    </div>
+    <section class="content">
+        <h1>Tis is  dash </h1>
+
+    </section>
+@endsection
---
+++ resources/views/layouts/dashboard/_aside.blade.php
@@ -0,0 +1,25 @@
+
+
+<Section class="slider">
+
+    <div class="sidebar">
+
+        <div class="user-panel"></div>
+
+
+    </div>
+
+
+    <div class="pull-left info">
+        <p>Alexander</p>
+    </div>
+    <ul class="sidebar-menu" data-widget="tree">
+       <li>
+        <a href="{{Route('dashboard.index')}}">
+            <i class="fa fa-th"></i>
+            <span>@lang('sait.dashboard')</span></a>
+
+        </li>
+    </ul>
+</Section>
+
