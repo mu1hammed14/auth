47c60ca095ecf70e4fd2dbdd85dd76aa4df962bc
nouran tariq <noo0ur.264@gmail.com>
2022-04-20 01:01:41 +0200

    dash2

---
+++ routes/Dashboard/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
+    ],
+    function(){
+
+    Route::prefix('dashboard')->name('dashboard.')->group(function ()
+    {
+        Route ::get('/index','DashboardController@index')->name('index');
+    });
+});
+
+
+
+
+
